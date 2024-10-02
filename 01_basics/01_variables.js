28cf3a5c3f743391fd9ef9ff9345e1ce66fdecd5
Varuni Balodhi <117430736+VaruniBalodhi@users.noreply.github.com>
2024-10-02 06:08:16 +0000

    Configure Commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountid=123
+var name='Varuni'
+let accemail='varu@gmail.com'
+console.log([accemail,accountid,name])
+//In java script we can change the datatype of the variables it is not restricted to the first datatype specifies
+//we can define using var and let.However var has become obsolate due to less control over block scope.let is more used because of more precision over scope
